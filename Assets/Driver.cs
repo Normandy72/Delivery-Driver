c71f4af192d8b5d68c56498f1c14b566a379f7e5
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-01-09 22:20:22 +0100

    feat: add points of speed changing

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -10,11 +10,8 @@ public class Driver : MonoBehaviour
     [SerializeField] float steerSpeed = 200f;
     [SerializeField] float moveSpeed = 18f;
 
-    
-    void Start()
-    {
-        
-    }
+    [SerializeField] float slowSpeed = 12f;
+    [SerializeField] float boostSpeed = 30f; 
 
     void Update()
     {
@@ -28,4 +25,19 @@ public class Driver : MonoBehaviour
         // движение по оси x, y, z
         transform.Translate(0, moveAmount, 0);
     }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "SpeedUp")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+
+    
 }
